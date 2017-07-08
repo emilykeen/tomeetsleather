7ba376aabd4187e812f620dfb05e6bc1a7d2dbfb
Emily Keen <emily.keen711@gmail.com>
2017-07-08 17:42:08 -0400

    added backend

---
+++ app/routing/htmlroutes.js
@@ -0,0 +1,31 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+  app.get("/aboutUs", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/html/AboutUs.html"));
+  });
+  app.get("/podcasts", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/html/podcasts.html"));
+  });
+  app.get("/blogs", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/html/Blogs.html"));
+  });
+      app.get("/blogs/Oct-11-2016", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/html/Blog/Oct112016.html"));
+  });
+  // If no matching route is found default to home
+  app.use('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/html/home.html"));
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+//app.use(express.static('app/public'));
+app.use(express.static(path.join(__dirname, 'app/public')));
+require("./app/routing/htmlRoutes")(app);
+
+// app.use('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, "/app/public/index.html"));
+//   });
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
